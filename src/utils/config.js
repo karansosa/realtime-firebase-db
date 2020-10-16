b9601d2953b28ab5d1038e38340500029343afff
Karan Sosa <karansosa1995@gmail.com>
2020-10-16 14:06:07 +0530

    first commit

---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+//TODO: DONE add firebase configuration and export it
+export const firebaseConfig = {
+  //TODO: ADD YOUR FIREBASE INFO HERE
+    apiKey: "AIzaSyDn9Oa5ZOiPfvbY68rxjfsgigBzogUw8Jw",
+    authDomain: "github-repository-7d0a2.firebaseapp.com",
+    databaseURL: "https://github-repository-7d0a2.firebaseio.com",
+    projectId: "github-repository-7d0a2",
+    storageBucket: "github-repository-7d0a2.appspot.com",
+    messagingSenderId: "651120524071",
+    appId: "1:651120524071:web:38b35d1a2e7339f90b8702",
+    measurementId: "G-RBHNJ730PB"
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
